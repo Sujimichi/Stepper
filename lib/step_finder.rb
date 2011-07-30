2a0dd4c5879ef1ed65c91444af5490021b686d36
sujimichi <sujimichi@googlemail.com>
2011-07-30 23:26:52 +0100

    comment and readme

--- lib/step_finder.rb
+++ lib/step_finder.rb
@@ -32,29 +32,42 @@ class StepFinder
     end
   end
 
+  #returns Given When Then or And if the given line starts with either of those strings
+  def determine_line_type line
+    return "Given" if line.match(/^Given/)
+    return "When" if line.match(/^When/)
+    return "Then" if line.match(/^Then/)
+    return "And" if line.match(/^And/)
+    return nil
+  end
+
+  #Find the step in Step which has the regexp that will match the given line.
   def find_step_which_matches line
-    type = determine_line_type(line)
+    type = determine_line_type(line)  #Determin type Given When Then or And
     steps = @steps.select{|s| s[:regexp].match(line.gsub("#{type} ","").chomp) } #Find the step which match the feature line
     raise "more than one match found" if steps.size >= 2
     return [steps.first, type] 
   end
 
+  #parse @step_files for lines with Given When and Then at the start and create an entry in @steps.
+  #each entry inclues a regexp that will function as the regexp defined by the step as will as file and linenuber info.
   def read_steps
     @steps = []
     @step_files.each do |file|
-      lines = File.open(file, "r"){|f| f.readlines}
-      lines.each_with_index do |line, index|
-        if line.match(/^Given|^When|^Then/)
-          type = determine_line_type(line)
-          l = line.split(" do")[0].gsub(/^#{type} /,"") #ignore everything after and including ' do' and remove Given, When or Then from start
-          l = l.sub("/","").reverse.sub("/","").reverse #remove / from begining and end of string so it will convert to regexp correctly
-          r_exp = Regexp.new(l)                         #create the regexp that will match (treated) lines in features
-          @steps << {:file => file, :line_number => index + 1, :line => line, :regexp => r_exp, :type => type} 
-        end
+      lines = File.open(file, "r"){|f| f.readlines}     #for each step_definition file, read the lines
+      lines.each_with_index do |line, index|            #for each line
+        next unless line.match(/^Given|^When|^Then/)    #Only interested in lines defining steps
+        type = determine_line_type(line)                #Get the type (Given, When Then) of the step
+        l = line.split(" do")[0].gsub(/^#{type} /,"")   #ignore everything after and including ' do' and remove Given, When or Then from start
+        l = l.sub("/","").reverse.sub("/","").reverse   #remove / from begining and end of string so it will convert to regexp correctly
+        r_exp = Regexp.new(l)                           #create the regexp that will match lines in features
+        @steps << {:file => file, :line_number => index + 1, :line => line, :regexp => r_exp, :type => type} #add step info to @steps
       end
     end
   end
 
+  #Parse each .feature file and for the lines with Given When Then or And, find the step which matches that line. 
+  #Each feature that matches a step is added to that steps' :features array.  So later a step can display the features which use it.
   def read_and_compaire_features
     @unmatched_features = []
     @feature_files.each do |file|
@@ -76,13 +89,6 @@ class StepFinder
     end
   end
 
-  def determine_line_type line
-    return "Given" if line.match(/^Given/)
-    return "When" if line.match(/^When/)
-    return "Then" if line.match(/^Then/)
-    return "And" if line.match(/^And/)
-    return nil
-  end
 
   def present_data
     output = ""
